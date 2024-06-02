4c9f97740c8c3d01e449a37a8ba9c19850ae2f2d
Lenerystia <77778018+Lenerystia@users.noreply.github.com>
2024-06-02 14:15:09 +0200

    dodanie zabezpieczenia by w dynamic routes wpisywać tylko integers (przynajmniej póki nie zechce ogarnąć uuid)

--- src/routes/[charId=integer]/+page.svelte
+++ src/routes/[charId=integer]/+page.svelte
@@ -2,9 +2,10 @@
     import '$lib/app.css'
     import { page } from '$app/stores';
     import { onMount } from 'svelte';
-    export let data;
+    export let data
+
     export let params;
-    
+
     let charId: string;
 
     charId = $page.params.charId;
@@ -19,4 +20,4 @@
 
 <h2>Fist Name: {data.result[0].firstName}</h2>
 <h2>Last Name: {data.result[0].lastName}</h2>
-<h2>ID {data.result[0].id}</h2>
+<h2>ID: {data.result[0].id}</h2>
