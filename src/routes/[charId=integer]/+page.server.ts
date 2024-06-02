ed1e84923a0dc4476a7d04ea8691930fc6b6d08c
Lenerystia <77778018+Lenerystia@users.noreply.github.com>
2024-06-02 20:19:51 +0200

    Thats work! I explicitly said that params contains the 'charId' string property, and then I parseInt it

--- src/routes/[charId=integer]/+page.server.ts
+++ src/routes/[charId=integer]/+page.server.ts
@@ -3,9 +3,10 @@ import { db } from '$lib/db';
 import { eq } from 'drizzle-orm';
 import type { PageServerLoad } from '../$types';
 
-export const load: PageServerLoad = (async ({params}) => {
-    const charId = params.charId;
-    const result = await db.select().from(characters).where(eq(characters.id, charId));
+export const load: PageServerLoad = (async ({params }) => {
+    const charId = (params as {charId: string}).charId;
+    const dupa = parseInt(charId);
+    const result = await db.select().from(characters).where(eq(characters.id, dupa));
     return {
        result
    };
